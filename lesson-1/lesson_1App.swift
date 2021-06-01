b80b90d12b3945db0dfd323813114d474ba6b1d2
Ludmila <adema@rambler.ru>
2021-06-01 10:56:43 +0300

    Initial Commit

---
+++ lesson-1/lesson_1App.swift
@@ -0,0 +1,17 @@
+//
+//  lesson_1App.swift
+//  lesson-1
+//
+//  Created by Людмила Бригаднова on 01.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct lesson_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
